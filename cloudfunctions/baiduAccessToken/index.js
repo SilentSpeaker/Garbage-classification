fed75dcbb2c46eff4db95194786eb8a9f6f685f2
ShenXi <821866219@qq.com>
2019-09-23 22:26:29 +0800

    new Branch LMJ

--- cloudfunctions/baiduAccessToken/index.js
+++ cloudfunctions/baiduAccessToken/index.js
@@ -1,12 +1,12 @@
 const rq = require('request-promise')
-// 
+//
 /**
  * 获取百度ai AccessToken
  */
 exports.main = async(event, context) => {
-  let apiKey = '2ypFOy3OTtK6QC8nG6SptEv1',
+  let apiKey = 'rcaTYoO3eTGgn19MO9VezPiy',
     grantType = 'client_credentials',
-    secretKey = 'tPPTP0m1cRxK1aXxEe3x206VtbuLF2Kb',
+    secretKey = '1WeHukKN8B5ulx0apPr1FLWuuXCuqyZb',
     url = `https://aip.baidubce.com/oauth/2.0/token`
   /* LMJ
   AppID='17321732'
